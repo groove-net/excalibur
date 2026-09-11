cab5b42b6a323c151595926ece278619ec1e76c7
Gabriel Adelemoni <gabadelemoni@outlook.com>
2026-09-11 09:30:12 -0400

    changed description

--- src/main.c
+++ src/main.c
@@ -2,9 +2,7 @@
  ============================================================================
  Name        : excalibur
  Version     : 1.0
- Description : A simple C application demonstrating project structure with
-               src, include, Makefile-based build system, and modular design.
-               Uses static libraries and includes dependency management.
+ Description : An simple and opinionated package manager for C
  ============================================================================
 */
 
